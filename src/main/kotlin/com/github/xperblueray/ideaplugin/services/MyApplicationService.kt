923e6fb66b4f5a67c920124e5ddacac2985dcbb4
GitHub Action <action@github.com>
2022-08-12 00:10:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/xperblueray/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xperblueray/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xperblueray.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xperblueray.ideaplugin.MyBundle
 
 class MyApplicationService {
 
